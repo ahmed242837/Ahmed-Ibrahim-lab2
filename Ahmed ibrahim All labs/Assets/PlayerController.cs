f1a46ff9cc819616a805aa8a2e8cd99477a848c6
ahmed242837 <ahmed242837@bue.edu.eg>
2025-10-12 03:46:37 -0700

    hi ahmed

---
+++ Ahmed ibrahim All labs/Assets/PlayerController.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;          // how fast the character moves
+    public float jumpHeight;         // how high the character jumps
+    public KeyCode Spacebar;         // Jump key (Spacebar)
+    public KeyCode L;                // Left movement key
+    public KeyCode R;                // Right movement key
+    public Transform groundCheck;    // an invisible point in space (used to detect ground)
+    public float groundCheckRadius;  // radius around ground check
+    public LayerMask whatIsGround;   // defines what is considered ground
+    private bool grounded;           // checks if character is standing on solid ground
+
+    void Start()
+    {
+        // Initialization (if needed)
+    }
+
+    void Update()
+    {
+        // Jump input
+        if (Input.GetKeyDown(Spacebar) && grounded)
+        {
+            Jump(); // call jump function
+        }
+
+        // Move left
+        if (Input.GetKey(L))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+            // move player left without affecting jump
+
+            if (GetComponent<SpriteRenderer>() != null)
+            {
+                GetComponent<SpriteRenderer>().flipX = true; // flip sprite to face left
+            }
+        }
+
+        // Move right
+        if (Input.GetKey(R))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+            // move player right without affecting jump
+
+            if(GetComponent<SpriteRenderer>()!= null)
+            {
+                GetComponent<SpriteRenderer>().flipX = false; // flip sprite to face right
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // Check if player is on ground
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+    }
+
+    void Jump()
+    {
+        // Make player jump vertically without affecting horizontal speed
+        GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+    }
+}
